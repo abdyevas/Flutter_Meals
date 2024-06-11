dcd64c9903a5d169e22d8444c22225c06f13b316
abdyevas <sabina.abdyeva@ufaz.az>
2024-06-11 23:35:06 +0400

    folders

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -9,13 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Recipies',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Text('Recipies'),
+    return const MaterialApp(
+      home: Text('Recipies')
     );
   }
 }
